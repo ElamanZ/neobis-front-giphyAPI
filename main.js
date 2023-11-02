163e9f73e1e8176da6312f47b0a6f05e0c755340
elamanz <z.elaman699@gmail.com>
2023-11-02 11:01:35 +0600

    project done

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
+const btnSearch = document.getElementById('btn-search');
+const inpSearch = document.getElementById('input-search');
+const resultsContainer = document.getElementById('giphy');
+const URL = "https://api.giphy.com/v1/gifs/search?api_key=";
+const APIKEY = "Bv3lnQoO2rIGMAXsYMGIpSvPSyTcCS6t";
 
-const btnSearch = document.getElementById('btn-search')
-const inpSearch = document.getElementById('input-search')
-const resultsContainer = document.getElementById('giphy')
-const URL = "https://api.giphy.com/v1/gifs/search?api_key="
-const APIKEY = "U531ZZAk6PTU6Z15Hy1H3KWdGijmUYcL"
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    btnSearch.addEventListener('click', async () => {
-        try {
-            const response = await fetch(`${URL}${APIKEY}&q=${inpSearch.value.trim()}&limit=25`);
-            const data = await response.json();
-            console.log('data', data);
-
-            // Очистите предыдущие результаты
-            resultsContainer.innerHTML = '';
-
-            // Обработайте данные и отобразите их на странице
-
-        } catch (e) {
-            console.error(e);
-        }
-    });
-
-
+document.addEventListener('DOMContentLoaded', async function () {
+    inpSearch.value = "cat";
+    searchGifs();
+    btnSearch.addEventListener('click', searchGifs);
 });
+async function searchGifs() {
+    btnSearch.disabled = true;
+
+    try {
+        const response = await fetch(`${URL}${APIKEY}&q=${inpSearch.value.trim()}&limit=24`);
+        const data = await response.json();
+
+
+        resultsContainer.innerHTML = '';
+
+        data.data.forEach(gif => {
+            const gifImage = document.createElement('img');
+            gifImage.src = gif.images.original.url;
+            resultsContainer.appendChild(gifImage);
+        });
+
+        inpSearch.value = '';
+    } catch (e) {
+        console.error(e);
+    } finally {
+        btnSearch.disabled = false;
+    }
+}
